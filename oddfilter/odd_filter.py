5109fa0a26f52c544fb681beacf1642bdd4e5503
Borbala Dobo <dobori@gmail.com>
2017-09-19 09:28:28 +0200

    feat(odd_filter.py): ready

--- oddfilter/odd_filter.py
+++ oddfilter/odd_filter.py
@@ -2,4 +2,14 @@
 # and returns a new list with every odd number from the orignal list
 # example: [1, 2, 3, 4, 5] should produce [1, 3, 5]
 
+old_list=[]
+newlist = []
+
+def odd_filter(old_list):
+    for i in old_list:
+        if i % 2 == 1:
+            newlist.append(i)
+    return(newlist)
+
+
 print(odd_filter([1, 2, 3, 4, 5]))  # should print [1, 3, 5]
