ef2470fddbba405472cbe015ee80604da85368af
Borbala Dobo <dobori@gmail.com>
2017-09-19 12:39:06 +0200

    feat(name_from_email): validation ready

--- namefromemail/name_from_email.py
+++ namefromemail/name_from_email.py
@@ -9,26 +9,20 @@
 
 #email_address = input("What is your email address?  (format: firstName.lastName@exam.com) ")
 email_address = ""
-
-# def is_valid_email(email_address):
-#     if re.match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$", email_address) != None:)
-#         return True
-#         print("This is a valid email address")
-#     else:
-#         return False
-#         print("This is not a valid email address")
-
-# def name_from_email():
-#     pass
-
-# is_valid_email("elek.vizexam.com")
-
 searching="@" 
 searching2="."
 
+def is_valid_email(email_address):
+    for i in email_address:
+        if searching in email_address:
+            return(True)
+
+
 def name_from_email(email_address):
-    end1 = email_address.index(searching)
-    end2 = email_address.index(searching2)
-    print(email_address[end2+1:end1], email_address[:end2] )
+    if is_valid_email(email_address):
+        end1 = email_address.index(searching)
+        end2 = email_address.index(searching2)
+        print(email_address[end2+1:end1], email_address[:end2] )
+
 
 print(name_from_email("elek.viz@exam.com"))
