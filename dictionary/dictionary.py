a2347a28aa66c4dc28de1aeb1deea55275ac0149
Borbala Dobo <dobori@gmail.com>
2017-09-19 12:16:02 +0200

    feat(dictionary): searcing is almost working

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -4,28 +4,35 @@ dictionary = [
 ]
 hun_word = ""
 eng_word = ""
+dict_item={}
 
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
 
 def add_word(hun_word, eng_word): 
-
-    dict_item={}
     dict_item[hun_word] = eng_word
     dictionary.append(dict_item)
-    #while hun_word != "" and eng_word != "":
-    #    count += 1
     return(dictionary)
 
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
 
-
-def translate_to_hun(eng_word):
-    pass
-
+# def translate_to_hun(eng_word): 
+#     value=""
+#     for i in dictionary:
+#         for value in dict_item:
+#             if eng_word == value:
+#                 hun_word = value[eng_word]
+#                 print(hun)      
 
 def translate_to_eng(hun_word):
-    pass
+    value=""
+    for i in dictionary:
+        for value in dict_item:
+            if hun_word == value:
+                eng = value[eng_word]
+            print(eng)  
 
 add_word("csinal", "make")
+#translate_to_hun("apple")
+translate_to_eng("alma")
