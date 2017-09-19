2f73f957cee8e523f2b13bef46813bbc8a1df330
Borbala Dobo <dobori@gmail.com>
2017-09-19 11:20:13 +0200

    feat(dictionary): add dictionary elements to a list ready

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -2,13 +2,20 @@ dictionary = [
     {"alma": "apple"},
     {"fa": "tree"}
 ]
+hun_word = ""
+eng_word = ""
 
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
 
+def add_word(hun_word, eng_word): 
 
-def add_word(hun_word, eng_word):
-    pass
+    dict_item={}
+    dict_item[hun_word] = eng_word
+    dictionary.append(dict_item)
+    #while hun_word != "" and eng_word != "":
+    #    count += 1
+    return(dictionary)
 
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
@@ -20,3 +27,5 @@ def translate_to_hun(eng_word):
 
 def translate_to_eng(hun_word):
     pass
+
+add_word("csinal", "make")
